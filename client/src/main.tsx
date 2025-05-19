b20f37efd258e115382c0a9e87936225093227e8
jay020420 <jayjay102@naver.com>
2025-05-19 12:50:00 +0900

    에러 수정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,11 +6,14 @@ import TanstackQueryProvider from './lib/tanstack-query';
 import { router } from './lib/router';
 import { Toaster } from '@/components/ui/sonner';
 
-createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Root element not found');
+
+const root = createRoot(rootElement);
+
+root.render(
   <TanstackQueryProvider>
     <RouterProvider router={router} />
     <Toaster position="top-center" richColors />
-  </TanstackQueryProvider>,
-  // </StrictMode>,
+  </TanstackQueryProvider>
 );
